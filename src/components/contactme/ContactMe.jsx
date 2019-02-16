fe5902d3c8eeb7a82a57d036e9eb2f5d9d5d1b30
Sheila Stephen <sheila.je@gmail.com>
2019-02-15 17:16:59 -0800

    add contact page

--- src/components/contactme/ContactMe.jsx
+++ src/components/contactme/ContactMe.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import linkedin from '../assets/images/Linkedin.png'
+import github from '../assets/images/gitthub.png'
+
+const imageStyles= {
+  width: '100px',
+  height: '100px',
+  marginRight: '40px'
+}
 
 function ContactMe()
 {   
   return(
-    <div>
-      <h1>Contact !!</h1>
+    <div style={{display: 'block'}}>
+      <div >
+        <h2>Contact</h2>
+        <p><a href="sheila.je@gmail.com" style={{fontSize: '40px'}}>sheila.je@gmail.com</a><br/></p>
+        <div>
+          <a href='https://www.linkedin.com/in/sheila-stephen/' target='blank' 
+            ><img src={linkedin} style= {imageStyles}/></a>
+          <a href='https://github.com/sheilaje' target='blank' style= {imageStyles}><img src={github}/></a>
+        </div>
+      </div>
     </div>
   )
 }
