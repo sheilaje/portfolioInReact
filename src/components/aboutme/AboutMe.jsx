a68b87536222763c826c51df86742010847083d4
Sheila Stephen <sheila.je@gmail.com>
2019-02-22 15:14:39 -0800

    add more content to teh aboutme page

--- src/components/aboutme/AboutMe.jsx
+++ src/components/aboutme/AboutMe.jsx
@@ -20,10 +20,10 @@ function AboutMe()
       <img  className = 'picStyles' src={profilepic} alt="Profile image"/>
       <div style={innerDivStyles}>
         {/* <h2 className = 'h2Styles' >About Me</h2> */}
-        <p className = 'pStyles'> I am a Full Stack Web Developer with C#, .NET, Node JS, Angular and React JS experience. I've worked as a lecturer in engineering colleges, and have a Masters in Engineering and Computer Science .I am seeking a software developer role where I can contribute, learn and grow my skills in web development.I am a adaptable, ambitious, self-motivated, confident and down to earth person. I have a great passion to learn. I love to be creative , and thats what led me to start my coding career at epicodus.</p>
-      </div>
-      <p className={'pStyles'} >My experience in Epicodus has taught me how to approach complex problems, write clean efficient code, learn new technology efficiently for the rapid evolution of the technology industry, and make contributions both independently and as a collaborator. 
+        <p className = 'pStyles'> I am a Full Stack Web Developer with C#, .NET, Node JS, Angular and React JS experience. I've worked as a lecturer in engineering colleges, and have a Masters in Engineering and Computer Science .I am seeking a software developer role where I can contribute, learn and grow my skills in web development.I am a adaptable, ambitious, self-motivated, confident and down to earth person. I have a great passion to learn. I love to be creative , and thats what led me to start my coding career at epicodus. My experience in Epicodus has taught me how to approach complex problems, write clean efficient code, learn new technology efficiently for the rapid evolution of the technology industry, and make contributions both independently and as a collaborator. 
       Embracing a growth mindset has allowed me to expand my knowledge and problem solving skills.</p>
+      </div>
+      <p className={'pStyles'} >Previously, As a lecturer I have handled students in class. I have been lecturing for basic Computer Science, that involves binary numbers, ASCII values, conversions. I have also taken classes in cryptography, which includes lots of algorithms for hiding messages and decrypting those messages back in the receiver side.I have also taken some Visual C++, Data Structures Lab. So I believe these things that I have learned from Epicodus and my previous experience will help me to be successful in my carreer as a software professional.</p>
     </div>
   )
 }
